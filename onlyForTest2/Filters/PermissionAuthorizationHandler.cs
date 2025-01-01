6eb7356fa21b82ecdcf79672c64b61c73017e0ca
Andrew Adel <Andrew_201900191@fci.helwan.edu.eg>
2025-01-01 13:17:43 +0200

    Add project files.

---
+++ onlyForTest2/Filters/PermissionAuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using OnlyForTest2.Filters;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlyForTest2.Filters
+{
+    public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        public PermissionAuthorizationHandler()
+        {
+
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            if (context.User == null)
+                return;
+
+            var canAccess = context.User.Claims.Any(c => c.Type == "Permission" && c.Value == requirement.Permission && c.Issuer == "LOCAL AUTHORITY");
+
+            if (canAccess)
+            {
+                context.Succeed(requirement);
+                return;
+            }
+        }
+    }
+}
